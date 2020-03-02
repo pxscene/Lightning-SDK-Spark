c4d960c62fee3c5e0cb950e346212543016525ee
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2020-03-02 23:44:48 +0200

    MediaPlayer, Lightning, ApplicationTexture

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ export * from 'lightning-sdk'
 
 // spark specific overrides
 export { default as MediaPlayer } from './src/MediaPlayer'
+export { default as ApplicationTexture } from './src/ApplicationTexture'
+export { default as Lightning } from './src/Lightning'
